5890d2bcca9d079e192daeb6c3b33aa288e7141a
Nele <neleemails@gmail.com>
2025-01-31 13:27:58 +0300

    metric crap ton of stuff

--- app/src/main/java/com/blackfox/app/AddressListArrayAdapter.java
+++ app/src/main/java/com/blackfox/app/AddressListArrayAdapter.java
@@ -15,26 +15,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class AddressListArrayAdapter extends RecyclerView.Adapter<AddressListArrayAdapter.ViewHolder> {
-
+public class AddressListArrayAdapter extends RecyclerView.Adapter<AddressListArrayAdapter.ViewHolder> implements AddressChoiceInterface {
     Context ctx;
     @NonNull
     ArrayList<String> addressList;
+    private final AddressChoiceInterface addressChoiceInterface;
 
     public AddressListArrayAdapter(Context ctx, @NonNull ArrayList<String> serverArrayList, AddressChoiceInterface addressChoiceInterface) {
         this.ctx = ctx;
         this.addressList = serverArrayList;
+        this.addressChoiceInterface = addressChoiceInterface;
     }
-
 
     @NonNull
     @Override
     public AddressListArrayAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
         LayoutInflater inflater = LayoutInflater.from(ctx);
         View view = inflater.inflate(R.layout.address_row, parent, false);
-
-        return new AddressListArrayAdapter.ViewHolder(view);
+        return new AddressListArrayAdapter.ViewHolder(view, addressChoiceInterface);
     }
 
     @Override
@@ -48,17 +46,27 @@ public class AddressListArrayAdapter extends RecyclerView.Adapter<AddressListArr
         return addressList.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    @Override
+    public void onItemclick(int position) {
+    }
 
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView addressText;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView, AddressChoiceInterface addressChoiceInterface) {
             super(itemView);
-
             addressText = itemView.findViewById(R.id.addressText);
-
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (addressChoiceInterface != null) {
+                        int pos = getAdapterPosition();
+                        if (pos != RecyclerView.NO_POSITION) {
+                            addressChoiceInterface.onItemclick(pos);
+                        }
+                    }
+                }
+            });
         }
-
-
     }
 }
